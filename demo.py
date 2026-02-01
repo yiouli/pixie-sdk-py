a8e25d8328b9c7c79b60a9bce28e5fcb6993098b
yiouli <liyiousu@gmail.com>
2026-02-01 00:24:33 -0800

    minor update to demo.py: import session functions from pixie.sdk

--- demo.py
+++ demo.py
@@ -1,12 +1,9 @@
 import asyncio
 
 import dotenv
-from pydantic import BaseModel
 from pydantic_ai import Agent, ModelMessage
 
 import pixie.sdk as pixie
-from pixie.server_utils import setup_logging
-from pixie.session import client
 
 
 @pixie.app
@@ -27,29 +24,23 @@ async def hello() -> pixie.PixieGenerator[str, str]:
 prompt = pixie.create_prompt("my_first_prompt", description="My first prompt")
 
 
-class Feedback(BaseModel):
-    rating: int
-    comments: str | None = None
-
-
-@client.session
+@pixie.session
 async def my_program():
     agent = Agent(model="gpt-4o-mini", system_prompt=prompt.compile())
-    await client.print("How can I assist you today? Type exit to end the conversation.")
+    await pixie.print("How can I assist you today? Type exit to end the conversation.")
     messages: list[ModelMessage] = []
     while True:
-        user_input = await client.input(expected_type=str)
+        user_input = await pixie.input(expected_type=str)
         if user_input == "exit":
             break
         response = await agent.run(
             user_input,
             message_history=messages,
         )
-        await client.print(response.output)
+        await pixie.print(response.output)
         messages = response.all_messages()
 
 
 if __name__ == "__main__":
-    setup_logging("debug")
     dotenv.load_dotenv()
     asyncio.run(my_program())
